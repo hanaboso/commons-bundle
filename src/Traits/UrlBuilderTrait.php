0cc407e5c66c5a74143508f32f6dc15b1abcf197
Radek Jirsa <jirsa.r@hanaboso.com>
2019-11-20 13:41:52 +0100

    Adde UrlBuilderTrait

---
+++ src/Traits/UrlBuilderTrait.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace Hanaboso\CommonsBundle\Traits;
+
+/**
+ * Trait UrlBuilderTrait
+ *
+ * @package Hanaboso\CommonsBundle\Traits
+ */
+trait UrlBuilderTrait
+{
+
+    /**
+     * @var string
+     */
+    protected $host;
+
+    /**
+     * @param string $part
+     * @param string ...$nestedPart
+     *
+     * @return string
+     */
+    protected function getUrl(string $part, string ...$nestedPart): string
+    {
+        return sprintf('%s/%s', rtrim($this->host, '/'), sprintf($part, ...$nestedPart));
+    }
+
+}
